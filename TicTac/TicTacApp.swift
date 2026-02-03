df70df40957ba73d7c7bffc042c72745aa14b676
pdzabaleta <pdzabaleta@gmail.com>
2026-02-03 19:28:00 -0300

    test

--- TicTac/TicTacApp.swift
+++ TicTac/TicTacApp.swift
@@ -1,14 +1,7 @@
-//
-//  TicTacApp.swift
-//  TicTac
-//
-//  Created by Pablo Daniel Zabaleta Maestre on 03-02-26.
-//
-
 import SwiftUI
 
 @main
-struct TicTacApp: App {
+struct TicTacToeSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
