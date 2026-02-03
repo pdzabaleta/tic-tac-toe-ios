4211a3dd10d3f746c0b27abcbbd31423018442a9
pdzabaleta <pdzabaleta@gmail.com>
2026-02-03 18:54:50 -0300

    Initial Commit

---
+++ TicTac/TicTacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacApp.swift
+//  TicTac
+//
+//  Created by Pablo Daniel Zabaleta Maestre on 03-02-26.
+//
+
+import SwiftUI
+
+@main
+struct TicTacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
